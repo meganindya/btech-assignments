a31701f0b8d47a92dfbcde2a32d56caee984488b
Anindya Kundu <anindya.k22@outlook.com>
2019-10-16 22:01:35 +0530

    Update Operating System file structure

--- operating-system/assg-1/1-time-sharing.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[]) { 
-    for(int i = 0; i < 10000; i++)
-        printf("%d\t%c\n", (i + 1), argv[1][0]);
-
-    return 0; 
-}
