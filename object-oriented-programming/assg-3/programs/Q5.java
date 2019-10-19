4dca639e9887e9c1f813b5d8e97f27f64ef072ab
Anindya Kundu <anindya.k22@outlook.com>
2019-10-19 16:40:35 +0530

    Add Q5.java

---
+++ object-oriented-programming/assg-3/programs/Q5.java
@@ -0,0 +1,28 @@
+public class Q5 {
+    public static void main(String args[]) {
+        U ob = new U();
+        ob.methodK();
+    }
+}
+
+
+interface K1 {
+    public static final int intK = 1;
+
+    public abstract void methodK();
+}
+
+interface K2 extends K1 {
+    public abstract void methodK();
+}
+
+interface K3 extends K2 {
+    public abstract void methodK();
+}
+
+
+class U implements K3 {
+    public void methodK() {
+        System.out.println(intK);
+    }
+}
