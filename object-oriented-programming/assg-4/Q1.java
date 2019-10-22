db1a9c83e4acf30b66fe8d1d619fe3bcfd4ec9e3
Anindya Kundu <anindya.k22@outlook.com>
2019-10-22 21:02:10 +0530

    Add Q1.java

---
+++ object-oriented-programming/assg-4/Q1.java
@@ -0,0 +1,48 @@
+import static java.lang.System.*;
+
+class Q1 {
+    public static void main(String args[]) {
+        Account accounts[] = new Account[5];
+        for (int i = 0; i < 5; i++)
+            accounts[i] = new Account(1001 + (int) (Math.random() * 5000));
+        
+        for (Account acc : accounts) {
+            out.println("=====\nBalance: " + acc.getBalance() + "\n");
+
+            acc.withdraw(500 + (int) (Math.random() * 25) * 100);
+            acc.withdraw(500 + (int) (Math.random() * 25) * 100);
+            acc.withdraw(500 + (int) (Math.random() * 25) * 100);
+        }
+    }
+}
+
+
+class Account {
+    private int balance;
+    
+    Account(int balance) {
+        this.balance = balance;
+    }
+
+    int getBalance() {
+        return balance;
+    }
+
+    void withdraw(int value) {
+        try {
+            if ((balance - value) < 1000) {
+                String temp = "Can't withdraw " + value + "\n";
+                throw new Exception(temp + "Minimum balance violated\n");
+            }
+            
+            else {
+                balance -= value;
+                out.println(value + " withdrawn");
+                out.println("New balance: " + balance + "\n");
+            }
+        }
+        catch (Exception e) {
+            out.println(e.getMessage());
+        }
+    }
+}
