1e203a2b0cb0ce8ad74ac82281c991a7516149e5
Anindya Kundu <anindya.k22@outlook.com>
2019-10-22 23:55:26 +0530

    Add Q5.java

---
+++ object-oriented-programming/assg-4/Q5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Q5 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\fEnter string: ");
+            try {
+                String s = br.readLine();
+
+                if (s.length() == 0)
+                    throw new NullPointerException("Error: Empty string\n");
+                else
+                    System.out.println(s + " entered");
+            }
+            catch (NullPointerException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+    }
+}
