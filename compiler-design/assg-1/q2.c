0fa574ecf059090cc21c97c47deb1832fefc196c
Anindya Kundu <anindya.k22@outlook.com>
2020-01-11 16:25:48 +0530

    Add comments

--- compiler-design/assg-1/q2.c
+++ compiler-design/assg-1/q2.c
@@ -1,22 +1,32 @@
+// inline comments: begin with // and stretch until end of line
+// multiline comments: enclosed between /* and */
+
+
 #include <stdio.h>
 #include <string.h>
 
 
 int main(int argc, char *argv[]) {
-    FILE *fi = fopen("input-2.txt", "r");
-    FILE *fo = fopen("output-2.txt", "w");
-    if (fi == NULL) goto exit;
+    FILE *fi = fopen("input-2.txt", "r");   // input file
+    FILE *fo = fopen("output-2.txt", "w");  // output file
+    if (fi == NULL)
+        goto exit;                  // exit if no input file
+
 
     char c;
     int state = 0;
-    char comment[1024]; int i = 0;
+    char comment[1024]; int i = 0;  // comment buffer and iterator
+
+    // loop for characters until END OF FILE
     while ((c = fgetc(fi)) != EOF) {
         switch (state) {
+            // initial state
             case 0:
                 if (c == '/')
                     state = 1;
             break;
 
+            // one / has been encountered
             case 1:
                 if (c == '/')
                     state = 2;
@@ -26,6 +36,7 @@ int main(int argc, char *argv[]) {
                     state = 0;
             break;
 
+            // two consecutive // have been encountered
             case 2:
                 if (c == '\n') {
                     comment[i++] = '\0';
@@ -36,6 +47,7 @@ int main(int argc, char *argv[]) {
                     comment[i++] = c;
             break;
 
+            // /* has been encountered
             case 3:
                 if (c == '*') {
                     comment[i++] = '*';
@@ -45,6 +57,8 @@ int main(int argc, char *argv[]) {
                     comment[i++] = c;
             break;
 
+            // * has been encountered after n number of
+            // characters have followed /*
             case 4:
                 if (c == '/') {
                     comment[i - 1] = '\0';
