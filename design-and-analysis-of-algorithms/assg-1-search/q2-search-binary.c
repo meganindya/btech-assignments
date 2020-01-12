94fad9f30a07f78554e3b0887b8a4a5f629c07f2
Anindya Kundu <anindya.k22@outlook.com>
2020-01-12 14:27:21 +0530

    Restructure question 2

--- design-and-analysis-of-algorithms/assg-1-search/q2-search-binary.c
+++ design-and-analysis-of-algorithms/assg-1-search/q2-search-binary.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 1000000000
 
+int getSearchBinaryTime(int n) {
+	long int *arr = malloc(sizeof(long int) * n);
+		for (long int i = 0; i < n; i++)
+			arr[i] = i;
+
+	int key = -1, iterations = 0;
+	
+	long int low = 0, high = n - 1;
+
+	//============================
+	while (high >= low) {
+		long int mid = low + ((high - low) / 2);
+		
+		if (arr[mid] == key)
+			break;
+		else if (arr[mid] < key)
+			low = mid + 1;
+		else
+			high = mid - 1;
+		
+		iterations++;
+	}
+	//============================
+
+	return iterations;
+}
 
 int main(int argc, char *argv) {
 	FILE *fp = fopen("q2-data.csv", "w");
 	long int n = 10000;
 	
-	while (n <= MAX_SIZE) {
-		long int *arr = malloc(sizeof(long int) * n), key = -1;
-		for (long int i = 0; i < n; i++)
-			arr[i] = i;
-
-		long int low = 0, high = n - 1;
-
-		int iterations = 0;
-
-		//============================
-		while (high >= low) {
-			long int mid = low + ((high - low) / 2);
-			
-			if (arr[mid] == key)
-				break;
-			else if (arr[mid] < key)
-				low = mid + 1;
-			else
-				high = mid - 1;
-			
-			iterations++;
-		}
-		//============================
-
-		fprintf(fp, "%ld,%d\n", n, iterations);
-
-
-		n <<= 1;
-	}
+	do {
+		fprintf(fp, "%ld,%d\n", n, getSearchBinaryTime(n));
+	} while((n <<= 1) < 1000000000);
 	
 	
 	fclose(fp);
