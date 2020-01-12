7d7e58076979dfabe8b268fe4e5a8f16899f7eca
Anindya Kundu <anindya.k22@outlook.com>
2020-01-13 01:34:29 +0530

    Update comments

--- design-and-analysis-of-algorithms/assg-1-search/q1-search-sequential.c
+++ design-and-analysis-of-algorithms/assg-1-search/q1-search-sequential.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>		// required for clock() calls
 
 
+// returns time taken (ns) by algorithm
 long int getSearchSequentialTime(int n) {
+	// array declared in heap, to avoid space restrictions
 	int *arr = (int *) malloc(sizeof(int) * n), key = -1;
 	for (int i = 0; i < n; i++)
 		arr[i] = i;
@@ -31,6 +33,7 @@ long int getSearchSequentialTime(int n) {
 	return (time * 1000000000);
 }
 
+// driver
 int main(int argc, char *argv) {
 	FILE *fp = fopen("q1-data.csv", "w");
 	long int n = 10000;
@@ -40,7 +43,7 @@ int main(int argc, char *argv) {
 	} while((n <<= 1) < 1000000000);
 	
 	fclose(fp);
-	system("./q1-plot.sh");
+	system("./q1-plot.sh");	// calls Bash script
 	
 	return 0;
 }
