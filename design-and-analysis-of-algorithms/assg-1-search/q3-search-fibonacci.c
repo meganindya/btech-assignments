188bf1afcdc6462a76353653897ba0e773af814b
Anindya Kundu <anindya.k22@outlook.com>
2020-01-13 01:12:47 +0530

    Add question 3 files

---
+++ design-and-analysis-of-algorithms/assg-1-search/q3-search-fibonacci.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int getSearchFibonacciTime(int n) {
+	int *arr = malloc(sizeof(int) * n);
+		for (long int i = 0; i < n; i++)
+			arr[i] = i;
+
+	//============================
+	int key = -1, iterations = 0;
+	
+	int fibM2 = 0, fibM1 = 1;
+	int fibM0 = fibM2 + fibM1;
+
+	while (fibM0 < n) {
+		fibM2 = fibM1;
+		fibM1 = fibM0;
+		fibM0 = fibM2 + fibM1;
+	}
+
+	int offset = -1;
+
+	while (fibM0 > 1) {
+		int i = (offset + fibM2) < (n - 1) ? (offset + fibM2) : (n - 1);
+
+		if (arr[i] < key) {
+			fibM0 = fibM1;
+			fibM1 = fibM2;
+			fibM2 = fibM0 - fibM1;
+			offset = i;
+		}
+
+		else if (arr[i] > key) {
+			fibM0 = fibM2;
+			fibM1 = fibM1 - fibM2;
+			fibM2 = fibM0 - fibM1;
+		}
+
+		else break;
+
+		iterations++;
+	}
+
+	if (fibM1 == 1 && arr[offset + 1] == key)
+		goto exit;
+	//============================
+
+	exit:
+	return iterations;
+}
+
+int main(int argc, char *argv) {
+	FILE *fp = fopen("q3-data.csv", "w");
+	long int n = 10000;
+	
+	do {
+		fprintf(fp, "%ld,%d\n", n, getSearchFibonacciTime(n));
+	} while((n <<= 1) < 1000000000);
+	
+	fclose(fp);
+	system("./q3-plot.sh");
+	
+	return 0;
+}
