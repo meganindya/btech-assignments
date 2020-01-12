8df4a89075d58361084be423d6c51dbb3aed59e7
Anindya Kundu <anindya.k22@outlook.com>
2020-01-12 14:34:45 +0530

    Restructure question 1

--- design-and-analysis-of-algorithms/assg-1-search/q1-search-sequential.c
+++ design-and-analysis-of-algorithms/assg-1-search/q1-search-sequential.c
@@ -3,41 +3,41 @@
 #include <time.h>
 
 
+long int getSearchSequentialTime(int n) {
+	int *arr = (int *) malloc(sizeof(int) * n), key = -1;
+	for (int i = 0; i < n; i++)
+		arr[i] = i;
+	
+	clock_t start, end;
+	start = clock();
+
+	//============================
+	int i = 1;
+
+	check:
+	if (key == arr[i - 1])
+		goto exit;
+	
+	i = i + 1;
+
+	if (i <= n)
+		goto check;
+	//============================
+
+	exit:
+	end = clock();
+	
+	double time = ((double) (end - start)) / CLOCKS_PER_SEC;
+	return (time * 1000000000);
+}
+
 int main(int argc, char *argv) {
 	FILE *fp = fopen("q1-data.csv", "w");
-	long int n = 10000, ti;
-	clock_t start, end;
+	long int n = 10000;
 	
-	while (n <= 100000000) {
-		int *arr = (int *) malloc(sizeof(int) * n), key = -1;
-		for (int i = 0; i < n; i++)
-			arr[i] = i;
-		
-		start = clock();
-
-		//============================
-		int i = 1;
-
-		check:
-		if (key == arr[i - 1])
-			goto exit;
-		
-		i = i + 1;
-
-		if (i <= n)
-			goto check;
-		//============================
-
-		exit:
-		end = clock();
-		
-		double time = ((double) (end - start)) / CLOCKS_PER_SEC;
-		ti = time * 1000000000;
-		fprintf(fp, "%ld,%ld\n", n, ti);
-
-
-		n <<= 1;
-	}
+	do {
+		fprintf(fp, "%ld,%ld\n", n, getSearchSequentialTime(n));
+	} while((n <<= 1) < 1000000000);
 	
 	
 	fclose(fp);
