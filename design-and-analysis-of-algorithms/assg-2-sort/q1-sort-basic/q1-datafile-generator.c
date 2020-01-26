75c00b5bf793c91457227cdc6457b4cd615ab654
Anindya Kundu <anindya.k22@outlook.com>
2020-01-26 17:33:48 +0530

    Initialize file sizes by reading from sizes.dat

--- design-and-analysis-of-algorithms/assg-2-sort/q1-sort-basic/q1-datafile-generator.c
+++ design-and-analysis-of-algorithms/assg-2-sort/q1-sort-basic/q1-datafile-generator.c
@@ -7,17 +7,23 @@
 
 
 int main(int argc, char *argv[]) {
-    int sizes[] = { 1000, 10000, 30000, 50000, 70000, 85000, 100000 };
-    int len = sizeof(sizes) / sizeof(int);
+    FILE *fp = fopen("./data-input/sizes.dat", "r");
+    int len = -1, temp;
+    while (!feof(fp)) { fscanf(fp, "%d", &temp); len++; }
+    rewind(fp);
+    int sizes[len];
+    for (int i = 0; i < len; i++)
+        fscanf(fp, "%d", &sizes[i]);
+    fclose(fp);
     
-    const char filepathtemp[] = "./data-input/size-00.csv";
+    const char filepathtemp[] = "./data-input/size-00.dat";
 
     for (int i = 0; i < len; i++) {
         char filepathcurr[24];
         strcpy(filepathcurr, filepathtemp);
         filepathcurr[18] = '0' + (i + 1) / 10;
         filepathcurr[19] = '0' + (i + 1) % 10;
-        FILE *fp = fopen(filepathcurr, "w");
+        fp = fopen(filepathcurr, "w");
 
         srand(time(0));
         for (int j = 0; j < sizes[i]; j++)
