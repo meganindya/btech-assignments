5762b0e249fba3655d67b17bc5b59273168f32f8
meganindya <anindya.k22@outlook.com>
2020-01-27 20:35:16 +0530

    Modify data set paths

--- design-and-analysis-of-algorithms/assg-2-sort/q1-sort-basic/q1-plot.sh
+++ design-and-analysis-of-algorithms/assg-2-sort/q1-sort-basic/q1-plot.sh
@@ -7,25 +7,25 @@ set terminal png size 1024,768
 set datafile separator ","
 
 set title "Basic Sorts (comparison)" font "Monospace Bold, 14"
-set output "./plots/q1-graph.png"
-plot "./data-output/q1a-data-sort-selection.csv" with lines lt 3 lw 2 linecolor 6,\
-     "./data-output/q1a-data-sort-selection.csv" with points lc rgb "black" pointtype 6 pointsize 1,\
-     "./data-output/q1b-data-sort-insertion.csv" with lines lt 3 lw 2 linecolor 1,\
-     "./data-output/q1b-data-sort-insertion.csv" with points lc rgb "black" pointtype 6 pointsize 1,\
-     "./data-output/q1c-data-sort-bubble.csv" with lines lt 3 lw 2 linecolor 2,\
-     "./data-output/q1c-data-sort-bubble.csv" with points lc rgb "black" pointtype 6 pointsize 1
+set output "./q1-plots/q1-graph.png"
+plot "./q1-data-output/q1a-data-sort-selection.csv" with lines lt 3 lw 2 linecolor 6,\
+     "./q1-data-output/q1a-data-sort-selection.csv" with points lc rgb "black" pointtype 6 pointsize 1,\
+     "./q1-data-output/q1b-data-sort-insertion.csv" with lines lt 3 lw 2 linecolor 1,\
+     "./q1-data-output/q1b-data-sort-insertion.csv" with points lc rgb "black" pointtype 6 pointsize 1,\
+     "./q1-data-output/q1c-data-sort-bubble.csv" with lines lt 3 lw 2 linecolor 2,\
+     "./q1-data-output/q1c-data-sort-bubble.csv" with points lc rgb "black" pointtype 6 pointsize 1
 
 set title "Selection Sort" font "Monospace Bold, 14"
-set output "./plots/q1a-graph-sort-selection.png"
-plot "./data-output/q1a-data-sort-selection.csv" with lines lt 3 lw 2 linecolor 7,\
-     "./data-output/q1a-data-sort-selection.csv" with points lc rgb "black" pointtype 6 pointsize 1
+set output "./q1-plots/q1a-graph-sort-selection.png"
+plot "./q1-data-output/q1a-data-sort-selection.csv" with lines lt 3 lw 2 linecolor 7,\
+     "./q1-data-output/q1a-data-sort-selection.csv" with points lc rgb "black" pointtype 6 pointsize 1
 
 set title "Insertion Sort" font "Monospace Bold, 14"
-set output "./plots/q1b-graph-sort-insertion.png"
-plot "./data-output/q1b-data-sort-insertion.csv" with lines lt 3 lw 2 linecolor 7,\
-     "./data-output/q1b-data-sort-insertion.csv" with points lc rgb "black" pointtype 6 pointsize 1
+set output "./q1-plots/q1b-graph-sort-insertion.png"
+plot "./q1-data-output/q1b-data-sort-insertion.csv" with lines lt 3 lw 2 linecolor 7,\
+     "./q1-data-output/q1b-data-sort-insertion.csv" with points lc rgb "black" pointtype 6 pointsize 1
 
 set title "Bubble Sort" font "Monospace Bold, 14"
-set output "./plots/q1c-graph-sort-bubble.png"
-plot "./data-output/q1c-data-sort-bubble.csv" with lines lt 3 lw 2 linecolor 7,\
-     "./data-output/q1c-data-sort-bubble.csv" with points lc rgb "black" pointtype 6 pointsize 1
+set output "./q1-plots/q1c-graph-sort-bubble.png"
+plot "./q1-data-output/q1c-data-sort-bubble.csv" with lines lt 3 lw 2 linecolor 7,\
+     "./q1-data-output/q1c-data-sort-bubble.csv" with points lc rgb "black" pointtype 6 pointsize 1
