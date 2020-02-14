d745c0ad8ff38cbec5e671f86d74cad3fca42ea4
meganindya <anindya.k22@outlook.com>
2020-02-14 15:21:13 +0530

    Update improved code

--- computer-networks/assg-1/java/TCPClient.java
+++ computer-networks/assg-1/java/TCPClient.java
@@ -1,40 +1,60 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class Client 
-{
-   public static void main(final String [] args) throws IOException 
-   {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final String serverName = "127.0.0.1";
-        final int port = 12345;
-        try 
-        {
-            System.out.println("Connecting to " + serverName + " on port " + port);
-            final Socket client = new Socket(serverName, port);
-         
-            System.out.println("Connected to " + client.getRemoteSocketAddress());
 
-            final OutputStream outToServer = client.getOutputStream();
-            final DataOutputStream out = new DataOutputStream(outToServer);
-            final InputStream inFromServer = client.getInputStream();
-            final DataInputStream in = new DataInputStream(inFromServer);
+public class TCPClient {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
-            System.out.println("Server says " + in.readUTF());
-            String inp = "";
-            while(inp.compareTo("exit")!=0)
-            {
-                inp = br.readLine();
-                
-            }
-         
-            
-            client.close();
-        } 
-        catch (final IOException e) 
-        {
+    public static void main(String args[]) throws IOException {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        final String serverAddr = "127.0.0.1";
+        final int port = 4200;
+
+        Socket conn = null;
+
+        try {
+            System.out.println("connecting to ''" + serverAddr + "' on port '" + port + "'");
+            conn = new Socket(serverAddr, port);
+
+            System.out.println("\n" + "connected to '" + conn.getRemoteSocketAddress().toString().substring(1) + "'\n");
+
+            final DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+            final DataInputStream inp  = new DataInputStream(conn.getInputStream());
+
+            chat(conn, inp, out);
+        }
+
+        catch (final IOException e) {
             e.printStackTrace();
         }
-   }
+
+        finally {
+            if (!conn.isClosed()) {
+                System.out.println("\n" + "connection with '" + conn.getRemoteSocketAddress().toString().substring(1) + "' ended");
+                conn.close();
+            }
+        }
+    }
+
+    public static void chat(Socket conn, DataInputStream inp, DataOutputStream out) throws IOException {
+        String message = "";
+
+        while (true) {
+            System.out.print("Client: ");
+            message = br.readLine();
+            out.writeUTF(message);
+
+            if (message.equalsIgnoreCase("bye"))
+                break;
+
+            message = inp.readUTF();
+            System.out.print("Server: ");
+            System.out.println(message);
+
+            if (message.equalsIgnoreCase("bye"))
+                break;
+        }
+    }
 }
--- computer-networks/assg-1/java/TCPServer.java
+++ computer-networks/assg-1/java/TCPServer.java
@@ -1,48 +1,83 @@
 import java.io.*;
+import java.util.*;
 import java.net.*;
 
 
-public class AckServer extends Thread
-{
-    private ServerSocket s;
-    public AckServer(int port) throws IOException
-    {
-        s = new ServerSocket(port,5);
-    }
-    public void run()
-    {
-        System.out.println("Server Started....Listening");
-        while(true)
-        {
-            try
-            {
-                Socket serv = s.accept();
-                System.out.println("Connected to " + serv.getRemoteSocketAddress());
-                DataInputStream in = new DataInputStream(serv.getInputStream());
-                DataOutputStream out = new DataOutputStream(serv.getOutputStream());
+public class TCPServer extends Thread {
+    private ServerSocket sock;
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-                System.out.println(in.readUTF());
-                out.writeUTF("Thank you for connecting to " + serv.getLocalSocketAddress()+ "\nGoodbye!");
-                serv.close();
+    public TCPServer(int port) throws IOException {
+        sock = new ServerSocket(port);
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("server listening on port '" + sock.getLocalPort() + "'");
+            Socket conn = null;
+
+            try {
+                conn = sock.accept();
+                System.out.println("\n" + "server connected to '" + conn.getRemoteSocketAddress().toString().substring(1) + "'\n");
+                DataInputStream inp  = new DataInputStream(conn.getInputStream());
+                DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+                chat(conn, inp, out);
+
+                System.out.print("\n" + "Exit? (Y/n): ");
+                if (sc.nextLine().equalsIgnoreCase("Y"))
+                    break;
+                else
+                    System.out.println();
             }
-            catch (IOException e) 
-            {
+
+            catch (IOException e) {
                 e.printStackTrace();
                 break;
-            } 
+            }
         }
     }
-    public static void main(String [] args) 
-    {
-        int port = 12345;
-        try 
-        {
-           Thread t = new AckServer(port);
-           t.start();
-        } 
-        catch (IOException e) 
-        {
-           e.printStackTrace();
+
+    public void chat(Socket conn, DataInputStream inp, DataOutputStream out) throws IOException {
+        String message = "";
+
+        while (true) {
+            message = inp.readUTF();
+            System.out.print("Client: ");
+            System.out.println(message);
+
+            if (message.equalsIgnoreCase("bye")) {
+                System.out.println("\n" + "connection with '" + conn.getRemoteSocketAddress().toString().substring(1) + "' ended");
+                conn.close();
+                break;
+            }
+
+            System.out.print("Server: ");
+            message = br.readLine();
+            out.writeUTF(message);
+
+            if (message.equalsIgnoreCase("bye")) {
+                System.out.println("\n" + "connection with '" + conn.getRemoteSocketAddress().toString().substring(1) + "' ended");
+                conn.close();
+                break;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        final int port = 4200;
+
+        try {
+            Thread t = new TCPServer(port);
+            t.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
