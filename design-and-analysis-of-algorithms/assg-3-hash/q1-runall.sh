295eba9cc70654034778e72554d4488c25954e51
meganindya <anindya.k22@outlook.com>
2020-03-02 20:20:18 +0530

    Add running script

---
+++ design-and-analysis-of-algorithms/assg-3-hash/q1-runall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ clear
+
+# GENERATE KEYS
+# =============
+# cc datafile-generator.c
+# ./a.out
+# rm a.out
+
+# STORE HASH TABLES
+# =================
+ cc q1-hash-store.c
+ ./a.out
+ rm a.out
