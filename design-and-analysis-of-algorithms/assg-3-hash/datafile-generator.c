9939061b13039409c2a8e367f728f37d575025c7
meganindya <anindya.k22@outlook.com>
2020-03-02 20:19:27 +0530

    Add key generator

---
+++ design-and-analysis-of-algorithms/assg-3-hash/datafile-generator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+    FILE *fp = fopen("./q1-data-input/keys.dat", "w+");
+
+    srand(time(0));
+    for (int i = 0; i < 100000; i++)
+        fprintf(fp, "%d\n", 1 + (int) (rand() % 65535));
+
+    fclose(fp);
+    return 0;
+}
