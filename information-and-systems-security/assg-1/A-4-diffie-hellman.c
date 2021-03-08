6b03b774f521442023e1b5b866fd52fe044592ca
Anindya Kundu <anindya.k22@outlook.com>
2021-03-08 15:53:26 +0530

    docs: add comments to A-4

--- information-and-systems-security/assg-1/A-4-diffie-hellman.c
+++ information-and-systems-security/assg-1/A-4-diffie-hellman.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <math.h>
 
-// returns value of a ^ b (mod P)
+// returns a ^ b (mod P)
 long long int power(long long int a, long long int b, long long int P)
 {
     return b == 1 ? a : (((long long int)pow(a, b)) % P);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     printf("\nDiffie-Hellman key exchange\n--------\n");
-    long long int P, G, x, a, y, b, ka, kb;
 
-    // Public keys G and P are agreed upon
-    P = 23; // A prime number P is taken
+    // Public keys (prime) P and (primitive root) G are agreed upon
+    long long int P = 23;
     printf("The value of P taken is %lld\n", P);
-
-    G = 9; // A primitve root for P, G is taken
+    long long int G = 9;
     printf("The value of G taken is %lld\n\n", G);
 
-    // Choose the private key a
+    long long int a;
+    // Private key a
     printf("Enter private key of a: ");
     scanf("%lld", &a);
-    x = power(G, a, P); // generated key from a
 
-    // Choose the private key b
+    long long int b;
+    // Private key b
     printf("Enter private key of b: ");
     scanf("%lld", &b);
-    y = power(G, b, P); // generated key from b
 
-    // Generating the secret key after the exchange of keys
-    ka = power(y, a, P); // Secret key for a
-    kb = power(x, b, P); // Secret key for b
+    printf("\n");
+    // Generated key from a
+    long long int x = power(G, a, P);
+    printf("Generated key from a is G^a (mod P) = %lld\n", x);
+    // Generated key from b
+    long long int y = power(G, b, P);
+    printf("Generated key from b is G^b (mod P) = %lld\n", y);
+
+    // Generate secret key after key exchange
+    long long int ka = power(y, a, P); // secret key for a
+    long long int kb = power(x, b, P); // secret key for b
 
     printf("\n");
     printf("Secret key for a is %lld\n", ka);
