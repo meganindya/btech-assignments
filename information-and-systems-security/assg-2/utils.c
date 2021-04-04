03aa653fee60d6d97bd4efdc1a02addfd8bdd5cf
Anindya Kundu <anindya.k22@outlook.com>
2021-04-04 20:46:59 +0530

    feat: add matrix inverse utilities

--- information-and-systems-security/assg-2/utils.c
+++ information-and-systems-security/assg-2/utils.c
@@ -81,3 +81,167 @@ int mod_26_mul_inv(int a)
 {
     return mod_mul_inv(a, 26);
 }
+
+/*
+ * Calculates the determinant of a matrix.
+ *
+ * ord_mat: order of the matrix
+ * mat: supplied matrix
+ *
+ * returns: determinant of mat
+ */
+int mat_determinant(int ord_mat, int **mat)
+{
+    if (ord_mat == 1)
+    {
+        return mat[0][0];
+    }
+
+    int det = 0;
+    for (int z = 0; z < ord_mat; z++)
+    {
+        int sign = ((1 - (z & 1)) << 1) - 1;
+
+        int **sub_mat;
+        sub_mat = malloc((ord_mat - 1) * sizeof *sub_mat);
+        for (int i = 0; i < ord_mat - 1; i++)
+        {
+            sub_mat[i] = malloc((ord_mat - 1) * sizeof *sub_mat[i]);
+        }
+
+        int kr = 0, kc = 0;
+        for (int r = 1; r < ord_mat; r++)
+        {
+            for (int c = 0; c < ord_mat; c++)
+            {
+                if (c != z)
+                {
+                    sub_mat[kr][kc] = mat[r][c];
+                    kc = (kc + 1) % ord_mat;
+                }
+            }
+            kr++;
+        }
+
+        det += sign * mat[0][z] * mat_determinant(ord_mat - 1, sub_mat);
+
+        for (int i = 0; i < ord_mat - 1; i++)
+        {
+            free(sub_mat[i]);
+        }
+        free(sub_mat);
+    }
+
+    return det;
+}
+
+/*
+ * Calculates and fills the cofactors for a matrix.
+ *
+ * ord_mat: order of the matrix
+ * mat: supplied matrix
+ * cof_mat: matrix to fill cofactors in
+ */
+void mat_cofactor(int ord_mat, int **mat, int **cof_mat)
+{
+    for (int r = 0; r < ord_mat; r++)
+    {
+        for (int c = 0; c < ord_mat; c++)
+        {
+            int sign = ((1 - ((r + c) & 1)) << 1) - 1;
+
+            int **sub_mat;
+            sub_mat = malloc((ord_mat - 1) * sizeof *sub_mat);
+            for (int i = 0; i < ord_mat - 1; i++)
+            {
+                sub_mat[i] = malloc((ord_mat - 1) * sizeof *sub_mat[i]);
+            }
+
+            int ki = 0, kj = 0;
+            for (int i = 0; i < ord_mat; i++)
+            {
+                for (int j = 0; j < ord_mat; j++)
+                {
+                    if (i != r && j != c)
+                    {
+                        sub_mat[ki][kj] = mat[i][j];
+                        kj = (kj + 1) % (ord_mat - 1);
+                    }
+                }
+                if (i != r)
+                    ki++;
+            }
+
+            cof_mat[r][c] = sign * mat_determinant(ord_mat - 1, sub_mat);
+
+            for (int i = 0; i < ord_mat - 1; i++)
+            {
+                free(sub_mat[i]);
+            }
+            free(sub_mat);
+        }
+    }
+}
+
+/*
+ * Transposes a square matrix in place.
+ *
+ * ord_mat: order of the matrix
+ * mat: supplied matrix
+ */
+void mat_transpose(int ord_mat, int **mat)
+{
+    for (int r = 0; r < ord_mat; r++)
+    {
+        for (int c = 0; c <= ((ord_mat - 1) >> 1); c++)
+        {
+            int temp = mat[r][c];
+            mat[r][c] = mat[c][r];
+            mat[c][r] = temp;
+        }
+    }
+}
+
+/*
+ * Checks whether a square matrix is invertible.
+ *
+ * ord_mat: order of the matrix
+ * mat: supplied matrix
+ * 
+ * Returns 0 if invertible; -1 otherwise.
+ */
+int mat_invert_check(int ord_mat, int **mat)
+{
+    /* Stores the determinant of the matrix. */
+    int det = mat_determinant(ord_mat, mat);
+    /* Stores the modular multiplicative inverse of the determinant. */
+    int det_inv = mod_26_mul_inv(det);
+
+    return det_inv == -1 ? -1 : 0;
+}
+
+/*
+ * Calculates the inverse (mod 26) of a matrix.
+ *
+ * ord_mat: order of the matrix
+ * mat: supplied matrix
+ * inv_mat: matrix to fill inverse in
+ */
+void mat_invert(int ord_mat, int **mat, int **inv_mat)
+{
+    /* Stores the determinant of the matrix. */
+    int det = mat_determinant(ord_mat, mat);
+    /* Stores the modular multiplicative inverse of the determinant. */
+    int det_inv = mod_26_mul_inv(det);
+
+    mat_cofactor(ord_mat, mat, inv_mat);
+    mat_transpose(ord_mat, inv_mat);
+
+    for (int i = 0; i < ord_mat; i++)
+    {
+        for (int j = 0; j < ord_mat; j++)
+        {
+            inv_mat[i][j] = mod_26(inv_mat[i][j] * det_inv);
+        }
+    }
+}
--- information-and-systems-security/assg-2/utils.h
+++ information-and-systems-security/assg-2/utils.h
@@ -6,3 +6,5 @@ int mod_add_inv(int, int);
 int mod_26_add_inv(int);
 int mod_mul_inv(int, int);
 int mod_26_mul_inv(int);
+int mat_invert_check(int, int **);
+void mat_invert(int, int **, int **);
